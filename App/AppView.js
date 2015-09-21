8f69514741fb48e19d5d874110a78c905a683769
Laura Knight <ljknight@gmail.com>
2015-09-21 16:58:37 -0700

    Move CurrentGIFView out of AppView and into its own view

--- App/AppView.js
+++ App/AppView.js
@@ -1,21 +1,14 @@
 var AppView = Backbone.View.extend ({
   el: '#container',
 
-  template: _.template('<a href="<%= sourceURL %>" target="window"><img src="<%= imageURL %>"/></a>'),
-
   initialize: function() {
-    // watches GIFs for a new add, re-renders page. this avoids the async rendering/ajax call.
-    this.listenTo(this.collection, 'add', this.render);
+    this.currentGIF = new CurrentGIFView({ collection: this.collection });
     this.button = new ButtonView({ collection: this.collection });
     this.render();
   },
 
   render: function() {
-    var gif = this.template({
-      imageURL: this.collection.models[0].attributes.imageURL,
-      sourceURL: this.collection.models[0].attributes.sourceURL
-    })
-    this.$el.append(gif, this.button.$el)
+    this.$el.append(this.currentGIF, this.button.$el)
     return this;
   }
 });
--- GIFs/CurrentGIFView.js
+++ GIFs/CurrentGIFView.js
@@ -1,13 +1,22 @@
+// only show last item in collection (most recent gif)
 var CurrentGIFView = Backbone.View.extend ({
   el: '#current',
 
+  template: _.template('<a href="<%= sourceURL %>" target="window"><img src="<%= imageURL %>"/></a>'),
+
   initialize: function() {
-    
+    // watches GIFs for a new add, re-renders page. this avoids the async rendering/ajax call.
+    this.listenTo(this.collection, 'add', this.render);
     this.render();
   },
 
   render: function() {
-
+    var gif = this.template({
+      // shows last image in the collection to make sure it's always showing the most recent
+      imageURL: this.collection.models[this.collection.models.length-1].attributes.imageURL,
+      sourceURL: this.collection.models[this.collection.models.length-1].attributes.sourceURL
+    });
+    this.$el.append(gif);
     return this;
   }
 });
